7fcce31e6e06621eb720d58b691422ffd9d90c4e
javiii93 <lindejavi93@gmail.com>
2020-01-17 11:39:23 +0100

    mainScreen and endScreen

--- core/src/com/badlogic/drop/Drop.java
+++ core/src/com/badlogic/drop/Drop.java
@@ -1,158 +1,29 @@
 package com.badlogic.drop;
 
-import java.awt.Dialog;
-import java.util.Iterator;
-
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.TimeUtils;
 
-public class Drop extends ApplicationAdapter {
-	private Texture dropImage;
-	private Texture bucketImage;
-	private Sound dropSound;
-	private Sound endGame;
-	private Music rainMusic;
-	private SpriteBatch batch;
-	private OrthographicCamera camera;
-	private Rectangle bucket;
-	private Array<Rectangle> raindrops;
-	private long lastDropTime;
-	private int puntuacion=0;
-	private String marcador="Score: "+puntuacion;
-	private SpriteBatch spriteBatch;
-	private BitmapFont mostrar;
 
-	@Override
-	public void create() {
-		// load the images for the droplet and the bucket, 64x64 pixels each
-		dropImage = new Texture(Gdx.files.internal("droplet.png"));
-		bucketImage = new Texture(Gdx.files.internal("bucket.png"));
-		mostrar=new BitmapFont();
-		// load the drop sound effect and the rain background "music"
-		dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
-		endGame = Gdx.audio.newSound(Gdx.files.internal("endGame.wav"));
-		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
+public class Drop extends Game {
 
-		// start the playback of the background music immediately
-		rainMusic.setLooping(true);
-		rainMusic.play();
+    public SpriteBatch batch;
+    public BitmapFont font;
 
-		// create the camera and the SpriteBatch
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 800, 480);
-		batch = new SpriteBatch();
-	spriteBatch=new SpriteBatch();
-		// create a Rectangle to logically represent the bucket
-		bucket = new Rectangle();
-		bucket.x = 800 / 2 - 64 / 2; // center the bucket horizontally
-		bucket.y = 20; // bottom left corner of the bucket is 20 pixels above the bottom screen edge
-		bucket.width = 64;
-		bucket.height = 64;
+    public void create() {
+        batch = new SpriteBatch();
+        //Use LibGDX's default Arial font.
+        font = new BitmapFont();
+        this.setScreen(new MainMenuScreen(this));
+    }
 
-		// create the raindrops array and spawn the first raindrop
-		raindrops = new Array<Rectangle>();
-		spawnRaindrop();
-	}
+    public void render() {
+        super.render(); //important!
+    }
 
-	private void spawnRaindrop() {
-		Rectangle raindrop = new Rectangle();
-		raindrop.x = MathUtils.random(0, 800-64);
-		raindrop.y = 480;
-		raindrop.width = 64;
-		raindrop.height = 64;
-		raindrops.add(raindrop);
-		lastDropTime = TimeUtils.nanoTime();
-	}
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
+    }
 
-	@Override
-	public void render() {
-		// clear the screen with a dark blue color. The
-		// arguments to glClearColor are the red, green
-		// blue and alpha component in the range [0,1]
-		// of the color to be used to clear the screen.
-		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		marcador="Score: "+puntuacion;
-		// tell the camera to update its matrices.
-		camera.update();
-
-		// tell the SpriteBatch to render in the
-		// coordinate system specified by the camera.
-		batch.setProjectionMatrix(camera.combined);
-
-		// begin a new batch and draw the bucket and
-		// all drops
-		batch.begin();
-		batch.draw(bucketImage, bucket.x, bucket.y);
-		for(Rectangle raindrop: raindrops) {
-			batch.draw(dropImage, raindrop.x, raindrop.y);
-		}
-		batch.end();
-	spriteBatch.begin();
-	//mostrar.draw(spriteBatch,marcador,25,953);
-		mostrar.draw(spriteBatch,marcador,720,480);
-	spriteBatch.end();
-		// process user input
-		if(Gdx.input.isTouched()) {
-			Vector3 touchPos = new Vector3();
-			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-			System.out.println(touchPos);
-			camera.unproject(touchPos);
-			bucket.x = touchPos.x - 64 / 2;
-		}
-		if(Gdx.input.isKeyPressed(Keys.LEFT)) bucket.x -= 200 * Gdx.graphics.getDeltaTime();
-		if(Gdx.input.isKeyPressed(Keys.RIGHT)) bucket.x += 200 * Gdx.graphics.getDeltaTime();
-
-		// make sure the bucket stays within the screen bounds
-		if(bucket.x < 0) bucket.x = 0;
-		if(bucket.x > 800 - 64) bucket.x = 800 - 64;
-
-		// check if we need to create a new raindrop
-		if(TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnRaindrop();
-
-		// move the raindrops, remove any that are beneath the bottom edge of
-		// the screen or that hit the bucket. In the latter case we play back
-		// a sound effect as well.
-		for (Iterator<Rectangle> iter = raindrops.iterator(); iter.hasNext(); ) {
-			Rectangle raindrop = iter.next();
-			raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
-			if(raindrop.y + 64 < 0){
-				iter.remove();
-				endGame.play();
-				System.out.println("fin del juego");
-
-			}
-
-			if(raindrop.overlaps(bucket)) {
-				dropSound.play();
-				puntuacion++;
-				iter.remove();
-			}
-		}
-	}
-
-	@Override
-	public void dispose() {
-		// dispose of all the native resources
-		dropImage.dispose();
-		bucketImage.dispose();
-		dropSound.dispose();
-		endGame.dispose();
-		rainMusic.dispose();
-		batch.dispose();
-	}
 }
---
+++ core/src/com/badlogic/drop/EndGameScreen.java
@@ -0,0 +1,114 @@
+package com.badlogic.drop;
+
+
+import com.badlogic.gdx.Gdx;
+
+import com.badlogic.gdx.Screen;
+
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.GL20;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
+
+public class EndGameScreen implements Screen {
+
+   final Drop game;
+
+    OrthographicCamera camera;
+    Sound endGame;
+    boolean b=true;
+
+    public EndGameScreen(final Drop gam) {
+
+        game = gam;
+        endGame = Gdx.audio.newSound(Gdx.files.internal("endGame.wav"));
+
+        camera = new OrthographicCamera();
+
+        camera.setToOrtho(false, 800, 480);
+
+
+    }
+
+
+    @Override
+
+    public void render(float delta) {
+
+        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+
+        camera.update();
+
+        game.batch.setProjectionMatrix(camera.combined);
+
+
+        game.batch.begin();
+        game.font.draw(game.batch, "Se ha acabado el juego Drop!!! ", 100, 150);
+
+        game.font.draw(game.batch, "Pulsa cualquier parte para volver a empezar!", 100, 100);
+
+        game.batch.end();
+if(b){
+    endGame.play();
+    b=false;
+}
+
+
+        if (Gdx.input.isTouched()) {
+
+            game.setScreen(new GameScreen(game));
+
+            dispose();
+
+        }
+
+    }
+
+
+    @Override
+
+    public void resize(int width, int height) {
+
+    }
+
+
+    @Override
+
+    public void show() {
+
+    }
+
+
+    @Override
+
+    public void hide() {
+
+    }
+
+
+    @Override
+
+    public void pause() {
+
+    }
+
+
+    @Override
+
+    public void resume() {
+
+    }
+
+
+    @Override
+
+    public void dispose() {
+        endGame.dispose();
+
+    }
+
+}
